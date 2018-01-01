abb71c7d27039161d3adccdca9dc581d36b0f91c
LiKneu <git@vthomas.de>
2018-01-01 20:05:44 +0100

    Table filter function added to list of comic series.

--- js/comicScripts.js
+++ js/comicScripts.js
@@ -32,3 +32,27 @@ function callPerlNewSeries () {
     // link of the perl script
     document.getElementById('lnk_create_series').href = seriesURL;
 }
+
+//  This function filters the table of all comic series according to the search
+//  string given in an input field.
+//  Function taken from: https://www.w3schools.com/howto/howto_js_filter_table.asp
+function filterSeriesTable() {
+  // Declare variables
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("searchInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("tableOfSeries");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
